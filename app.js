81044beb238c7f48e5df77ab040fe016e41d12b6
ibrahim-1234 <aabr333@hotmail.com>
2023-03-22 14:52:43 +0300

    make website dynamic using ejs and adding routers folder to handle routes

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 import express from 'express'
-const router = express.Router()
+import router from './routes/home.js'
 
+ 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
+app.set('view engine', 'ejs')
 app.use(express.static('views'))
-
-router.get('/', (req, res)=>{
-    res.render('index')
-})
-
 app.use('/', router)
 
 app.listen(PORT, ()=>{
     console.log(`http://localhost:${PORT}`);
 })
-
-// "start": "lite-server --baseDir=\"views\""
