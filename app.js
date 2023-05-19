3f030abfec8b00366f70cc3e1d97dfb3e423cf95
ibrahim-1234 <117840775+ibrahim-1234@users.noreply.github.com>
2023-05-19 23:01:17 +0000

    a

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import router from "./controllers/home.js";
 
+const host = 'http://192.168.100.122:'
+// const localhost = 'http://localhost:'
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,5 +12,5 @@ app.use(express.static("views"));
 app.use("/", router);
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`${host}${PORT}`);
 });
